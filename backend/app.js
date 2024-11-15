0e6b3d6382c2fa430e6d26eb7ceafb883de77f92
LC0229 <shc067@ucsd.edu>
2024-11-14 17:25:53 -0800

    Setting MongoDB

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const connectDB = require('./db');
+require('dotenv').config();
+connectDB();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
