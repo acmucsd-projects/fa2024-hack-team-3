fb2e74ed160fd08b923c15649d973fc88f0ae2c6
Saianshul <vishnubhaktula@gmail.com>
2024-11-21 00:38:09 -0800

    update register and login pages

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from '../login_register_components/Navbar';
+import NavButton from '../login_register_components/NavButton';
+import { Box, Container, Stack } from '@chakra-ui/react';
+// import ImageSection from '../login_register_components/ImageSection';
+
 const RegisterPage = () => {
     return (
-        <Navbar />
+        <Box>
+            <Navbar />
+            <Box display={'flex'} justifyContent={'center'} alignItems={'center'} minHeight="calc(100vh - 60px)" bg="gray.100">
+                <Stack direction={{base: 'column', md: 'row'}} maxW="5xl" borderRadius="lg" overflow="hidden" bg="white">
+                    <>HELLO</>
+                    <>HI</>
+                    <>YES</>
+                </Stack>
+            </Box>
+        </Box>
     )
 }
 
