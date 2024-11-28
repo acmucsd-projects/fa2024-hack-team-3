148b84ffff7837d124d2a563f5c8ea11d3442f22
Saianshul <vishnubhaktula@gmail.com>
2024-11-27 16:54:33 -0800

    handle get, post, delete, and patch requests for users

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,16 +1,102 @@
 const User = require('../models/userModel');
+// const mongoose = require('mongoose');
+
+const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find({}).sort({ createdAt: -1 });
+        res.status(200).json(users);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+}
+
+const getUser = async (req, res) => {
+    const { id } = req.params;
+    /*
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({ error: 'Not a valid id' });
+    }
+
+    const user = await User.findById({ _id: id });
+
+    if (!user) {
+        return res.status(400).json({ error: 'Cannot get this user' });
+    }
+
+    res.status(200).json(user);
+    */
+    try {
+        const user = await User.findById({ _id: id });
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+}
 
 const createUser = async (req, res) => {
-    const { username, emailAddress, courses } = req.body;
+    const { username, emailAddress, courses, anonymous } = req.body;
 
     try {
-        const user = await User.create({username, emailAddress, courses});
+        const user = await User.create({ username, emailAddress, courses, anonymous });
         res.status(201).json(user);
     } catch (err) {
-        res.status(500).json({ error: err.message });
+        res.status(400).json({ error: err.message });
+    }
+}
+
+const deleteUser = async (req, res) => {
+    const { id } = req.params;
+    /*
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({ error: 'Not a valid id' });
+    }
+
+    const user = await User.findOneAndDelete({ _id: id });
+
+    if (!user) {
+        return res.status(400).json({ error: 'Cannot delete this user' });
+    }
+
+    res.status(200).json(user);
+    */
+
+    try {
+        const user = await User.findOneAndDelete({ _id: id });
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+}
+
+const updateUser = async (req, res) => {
+    const { id } = req.params;
+
+    /*
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({ error: 'Not a valid id' });
+    }
+
+    const user = await User.findOneAndUpdate({ _id: id }, { ...req.body });
+
+    if (!user) {
+        return res.status(400).json({ error: 'Cannot update this user' });
+    }
+
+    res.status(200).json(user);
+    */
+
+    try {
+        const user = await User.findOneAndUpdate({ _id: id }, { ...req.body });
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
     }
 }
 
 module.exports = {
-    createUser
+    getAllUsers,
+    getUser,
+    createUser,
+    deleteUser,
+    updateUser
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,15 @@
 const express = require('express');
-const { createUser } = require('../controllers/userControllers');
+const { getAllUsers, getUser, createUser, deleteUser, updateUser } = require('../controllers/userControllers');
 
 const router = express.Router();
 
+router.get('/', getAllUsers);
+router.get('/:id', getUser);
+
 router.post('/', createUser);
+
+router.delete('/:id', deleteUser);
+
+router.patch('/:id', updateUser);
 
 module.exports = router;
