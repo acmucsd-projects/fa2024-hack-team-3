288ae6c33a737f81a2136dfc048849539c92e856
emilyycaii <e1cai@ucsd.edu>
2024-12-09 22:13:57 -0800

    created account header for account settings page

--- frontend/src/account_settings_components/AccountHeader.jsx
+++ frontend/src/account_settings_components/AccountHeader.jsx
@@ -1,14 +1,21 @@
 import React from "react";
-import { Flex, Avatar, FormControl, FormLabel, Input } from "@chakra-ui/react";
+import { Flex, VStack, Input, Container, Text } from "@chakra-ui/react";
+import { Avatar} from "../components/ui/avatar"
 
 const AccountHeader = () => {
   return (
-    <Flex direction="column" align="center" mb={8}>
-      <Avatar size="2xl" mb={4} />
-      <FormControl>
-        <FormLabel>Username</FormLabel>
-        <Input placeholder="Insert username" />
-      </FormControl>
+    <Flex
+      direction="row"
+      justify="center" // Centers horizontally
+      h="100vh" // Full screen height
+    >
+        <Avatar size="2xl" mb={4} />
+        <VStack align="center" ml={6}> {/* Adds spacing between Avatar and the inputs */}
+            <Text fontSize="lg" fontWeight="bold">
+                Username
+            </Text>
+            <Input placeholder="Insert username" w="200px" /> {/* Adjust width as needed */}
+        </VStack>
     </Flex>
   );
 };
--- frontend/src/pages/AccountSettings.jsx
+++ frontend/src/pages/AccountSettings.jsx
@@ -2,17 +2,17 @@
 import React from "react";
 import { Flex} from "@chakra-ui/react";
 import Sidebar from "../account_settings_components/Sidebar";
-//import AccountHeader from "../account_settings_components/AccountHeader";
+import AccountHeader from "../account_settings_components/AccountHeader";
 //import AccountForm from "../account_settings_components/AccountForm";
 //import CoursesSection from "../account_settings_components/CoursesSection";
 //import LogoutButton from "../account_settings_components/LogoutButton";
 
 const AccountSettings = () => {
   return (
-    <>
-        <p> "Hello" </p>
+    <Flex direction="row" h="100vh" p={4} bg="gray.50">
         <Sidebar />
-    </>
+        <AccountHeader />
+    </Flex>
   );
 };
 
