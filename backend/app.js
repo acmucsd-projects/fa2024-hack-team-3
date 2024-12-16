acb66328d1af90da7da8196efb16d7777f998f2f
saianshul <vishnubhaktula@gmail.com>
2024-12-16 03:51:00 -0700

    add more controllers to userControllers, remove controller functions from postRoutes and add them to postControllers, make minor changes to code but don't change functionality

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const dotenv = require("dotenv");
 const cors = require("cors");
 const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 // Routes
-//const authRoutes = require('./routes/authRoutes'); // Import your auth routes
+// const authRoutes = require('./routes/authRoutes'); // Import your auth routes
 const userRoutes = require('./routes/userRoutes');
 const postRoutes = require('./routes/postRoutes');
-const Post = require('./models/userPost'); // Ensure this path is correct
 const commentRoutes = require('./routes/commentRoutes');
-
-
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 const app = express();
 
-// set strict query mode for Mongoose
-mongoose.set('strictQuery', true);
+app.use(express.json());
 
 app.use(cors({
     origin: 'http://localhost:5173', // Allow requests only from this origin (your frontend)
 }));
 
-app.use(express.json());
+// set strict query mode for Mongoose
+mongoose.set('strictQuery', true);
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
@@ -38,14 +34,7 @@ mongoose.connect(process.env.MONGO_URI)
     });
 
 // Register API routes
-//app.use('/api/auth', authRoutes); // Link the Google login handler
+// app.use('/api/auth', authRoutes); // Link the Google login handler
 // app.use('/api/posts', PostRoutes); // For posts
-
-
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
 
 module.exports = app;
