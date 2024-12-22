71689f19df17e8cfc8ab87967e81a767ca011140
saianshul <vishnubhaktula@gmail.com>
2024-12-22 01:28:21 -0700

    implement basic functionality for option to add courses in the register page

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,32 +1,28 @@
-// import React from 'react';
 import { useState } from 'react';
-import Navbar from '../login_register_components/Navbar';
+import { useNavigate, Link } from 'react-router-dom';
+import Select from 'react-select';
+import axios from 'axios';
 import { Box, Container, Flex, Image, VStack, Heading, Text, Button} from '@chakra-ui/react';
 import { Toaster, toaster } from '../components/ui/toaster';
-import logo from '../assets/logo.svg';
+import Navbar from '../login_register_components/Navbar';
 import InputField from '../login_register_components/InputField';
-// import ImageSection from '../login_register_components/ImageSection';
-import {GoogleLogin} from '@react-oauth/google';
-import { jwtDecode } from 'jwt-decode';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import logo from '../assets/logo.svg';
 
 const RegisterPage = () => {
-
     // State for form fields
     const [formData, setFormData] = useState({
         username: "",
         email: "",
         password: "",
-        confirmPassword: ""
+        confirmPassword: "",
+        courses: [],
     });
 
     const [validationErrors, setValidationErrors] = useState({
         username: "",
         email: "",
         password: "",
-        confirmPassword: ""
+        confirmPassword: "",
     });
 
     const navigate = useNavigate();
@@ -38,49 +34,67 @@ const RegisterPage = () => {
                 if (!/^[a-zA-Z0-9]{3,15}$/.test(value)) {
                     return "Username must be 3-15 characters long, letters and numbers only";
                 }
+
                 try {
                     const response = await axios.post("http://localhost:5000/api/users/check-username", { username: value });
+
                     if (response.data.exists) {
                         return "Username is already taken";
                     }
                 } catch (err) {
                     console.error(err);
                 }
+
                 return "";
             case "email":
                 if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)) {
                     return "Please enter a valid email address";
                 }
+
                 try {
                     const response = await axios.post("http://localhost:5000/api/users/check-email", { emailAddress: value });
+
                     if (response.data.exists) {
                         return "Email is already taken";
                     }
                 } catch (err) {
                     console.error(err);
                 }
+
                 return "";
             case "password":
                 if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(value)) {
                     return "Password must be at least 8 characters long, contain an uppercase letter, a lowercase letter, a number, and a special character.";
                 }
+
                 return "";
             case "confirmPassword":
                 if (value !== formData.password) {
                     return "Passwords do not match";
                 }
+
                 return "";
             default:
                 return "";
         }
-    };
+    }
 
     // Handle input changes
     const handleChange = async (e) => {
         const { name, value } = e.target;
 
+        // Updates form data separately if data to be updated is courses
+        if (name === "courses") {
+            setFormData((prevData) => ({
+                ...prevData,
+                courses: value.map((course) => ({ name: course.value })), // Assuming value is the array of selected options
+            }));
+
+            return;
+        }
+
         // Update form data
-        setFormData({ ...formData, [name]: value}); // Update the form data
+        setFormData({ ...formData, [name]: value }); // Update the form data
         
         // Validate field and update validation errors
         const errorMessage = await validateField(name, value);
@@ -89,19 +103,20 @@ const RegisterPage = () => {
 
     // Handle form submission
     const handleSubmit = async () => {
-        const { username, email, password, confirmPassword } = formData;
+        const { username, email, password, confirmPassword, courses } = formData;
 
         console.log({
             username,
             emailAddress: email,
             password,
-        })
+            courses,
+        });
 
         const errors = {
             username: await validateField("username", username),
             email: await validateField("email", email),
             password: await validateField("password", password),
-            confirmPassword: await validateField("confirmPassword", confirmPassword)
+            confirmPassword: await validateField("confirmPassword", confirmPassword),
         }
 
         setValidationErrors(errors);
@@ -127,6 +142,7 @@ const RegisterPage = () => {
                 username,
                 emailAddress: email,
                 password,
+                courses,
             });
 
             // Show success toast
@@ -136,7 +152,7 @@ const RegisterPage = () => {
                 status: "success",
                 duration: 3000,
                 isClosable: true,
-                type: "success"
+                type: "success",
             });
 
             // Clear form data
@@ -144,7 +160,8 @@ const RegisterPage = () => {
                 username: "",
                 email: "",
                 password: "",
-                confirmPassword: ""
+                confirmPassword: "",
+                courses: [],
             });
 
             setTimeout(() => {navigate("/login")}, 2000);
@@ -158,10 +175,17 @@ const RegisterPage = () => {
                 duration: 3000,
                 isClosable: true,
                 type: "error",
-                overlap: true
+                overlap: true,
             });
         }
-    };
+    }
+
+    const courses = [
+        { value: 'CSE 11', label: 'CSE 11 - Introduction to Computer Science' },
+        { value: 'MATH 20C', label: 'MATH 20C - Calc III' },
+        { value: 'ECON 1', label: 'ECON 1 - Principles of Microeconomics' },
+        { value: 'CSe 12', label: 'CSE 12 - Data Structures' },
+    ];
 
     return (
         <Box>
@@ -177,104 +201,135 @@ const RegisterPage = () => {
             >
                 {/* Form Section */}
                 <Container
-                    flex={{ base: 2}}
+                    flex={{ base: 2 }}
                     p={8}
                     borderRadius="lg"
                     mx="4"
-                    h={{base: "100vh", md: "90%"}}
+                    h={{ base: "100vh", md: "90%" }}
                 >
-                    <VStack spacing={4} align="flex-start" px={{base: 4, lg: 10}} ml={{lg: "0"}}>
-                            <Heading size={"3xl"} mb={2} textAlign="left" color={"black"} fontWeight={"bold"}>
-                                Join StudyLink Today!
-                            </Heading>
-                            <Text color={"gray.800"} textAlign={"left"} fontSize={"xl"} fontWeight={"medium"} maxW={{lg: "80%"}}>
-                                {/* Stuck on an assignment? Or need someone to motivate you to keep studying? Come study with us! */}
-                                Create your account and start connecting with study buddies!
-                            </Text>
+                    <VStack spacing={4} align="flex-start" px={{ base: 4, lg: 10 }} ml={{ lg: "0" }}>
+                        <Heading size={"3xl"} mb={2} textAlign="left" color={"black"} fontWeight={"bold"}>
+                            Join StudyLink Today!
+                        </Heading>
 
-                            {/* Username Field */}
-                            <InputField 
-                                label={"Username"}
-                                name={"username"}
-                                value={formData.username}
-                                onChange={handleChange}
-                                color={"gray.800"}
-                                required
-                                width={{base: "100%", lg: "80%"}}
-                            />
-                            {validationErrors.username && (
-                                <Text color="red.500" fontSize="sm">{validationErrors.username}</Text>
+                        <Text color={"gray.800"} textAlign={"left"} fontSize={"xl"} fontWeight={"medium"} maxW={{ lg: "80%" }}>
+                            {/* Stuck on an assignment? Or need someone to motivate you to keep studying? Come study with us! */}
+                            Create your account and start connecting with study buddies!
+                        </Text>
+
+                        {/* Username Field */}
+                        <InputField 
+                            label={"Username"}
+                            name={"username"}
+                            value={formData.username}
+                            onChange={handleChange}
+                            color={"gray.800"}
+                            required
+                            width={{ base: "100%", lg: "80%" }}
+                        />
+                        {validationErrors.username && (
+                            <Text color="red.500" fontSize="sm">{validationErrors.username}</Text>
+                        )}
+
+                        {/* Email Field */}
+                        <InputField 
+                            label={"Email"} 
+                            name={"email"}
+                            value={formData.email}
+                            onChange={handleChange}
+                            color={"gray.800"} 
+                            required 
+                            width={{ base: "100%", lg: "80%" }}
+                        /> 
+                        {validationErrors.email && (
+                            <Text color="red.500" fontSize="sm">{validationErrors.email}</Text>
+                        )}
+
+                        {/* Password Field */}
+                        <InputField 
+                            label={"Password"} 
+                            name={"password"}
+                            type={"password"}
+                            value={formData.password}
+                            onChange={handleChange}
+                            color={"gray.800"} 
+                            required 
+                            width={{ base: "100%", lg: "80%" }}
+                        />
+                        {validationErrors.password && (
+                            <Text color="red.500" fontSize="sm">{validationErrors.password}</Text>
+                        )}
+
+                        {/* Confirm Password Field */}
+                        <InputField 
+                            label={"Confirm Password"} 
+                            name={"confirmPassword"}
+                            type={"password"}
+                            value={formData.confirmPassword}
+                            onChange={handleChange}
+                            color={"gray.800"} 
+                            required 
+                            width={{ base: "100%", lg: "80%" }}
+                        />
+                        {validationErrors.confirmPassword && (
+                            <Text color="red.500" fontSize="sm">{validationErrors.confirmPassword}</Text>
+                        )}
+
+                        <Text color="gray.800">Courses</Text>
+                        <Select
+                            options={courses}
+                            isMulti
+                            closeMenuOnSelect={false}
+                            placeholder="Select courses you are currently enrolled in..."
+                            value={formData.courses.map((course) =>
+                                courses.find((option) => course.name === option.value)
                             )}
+                            onChange={(selectedCourses) =>
+                                handleChange({
+                                    target: { name: "courses", value: selectedCourses }
+                                })
+                            }
+                            isSearchable={true}
+                            styles={{
+                                container: (base) => ({ ...base, width: '80%' }),
+                                menu: (base) => ({
+                                    ...base,
+                                    maxHeight: '120px',
+                                    overflowY: 'auto',
+                                }),
+                                menuList: (base) => ({
+                                    ...base,
+                                    maxHeight: '120px',
+                                    overflowY: 'auto',
+                                }),
+                            }}
+                        />
+                        
+                        <Box width={{ base: "100%", lg: "80%" }} textAlign="center" mt="1em" >
+                            <Button variant="solid" bg={'blue.800'} _hover={{ bg: "blue.700" }} width="100%" onClick={handleSubmit}>
+                                <Text fontWeight={"bold"}>REGISTER</Text>
+                            </Button>
+                        </Box>
 
-                            {/* Email Field */}
-                            <InputField 
-                                label={"Email"} 
-                                name={"email"}
-                                value={formData.email}
-                                onChange={handleChange}
-                                color={"gray.800"} 
-                                required 
-                                width={{base: "100%", lg: "80%"}}
-                            /> 
-                            {validationErrors.email && (
-                                <Text color="red.500" fontSize="sm">{validationErrors.email}</Text>
-                            )}
+                        <Text textAlign={"left"} color={'gray.500'} pt={4}>
+                            Already have an account? <Text as={Link} to={'/login'} textDecoration="underline">Login</Text>
+                        </Text>
+                        {/* <GoogleLogin 
+                            onSuccess={(credentialResponse) => {
+                                const decoded = jwtDecode(credentialResponse.credential);
+                                console.log(decoded);
+                            }}
+                            onError={() => {
+                                console.log("Login Failed");
+                            }}
 
-                            {/* Password Field */}
-                            <InputField 
-                                label={"Password"} 
-                                name={"password"}
-                                type={"password"}
-                                value={formData.password}
-                                onChange={handleChange}
-                                color={"gray.800"} 
-                                required 
-                                width={{base: "100%", lg: "80%"}}
-                            />
-                            {validationErrors.password && (
-                                <Text color="red.500" fontSize="sm">{validationErrors.password}</Text>
-                            )}
-
-                            {/* Confirm Password Field */}
-                            <InputField 
-                                label={"Confirm Password"} 
-                                name={"confirmPassword"}
-                                type={"password"}
-                                value={formData.confirmPassword}
-                                onChange={handleChange}
-                                color={"gray.800"} 
-                                required 
-                                width={{base: "100%", lg: "80%"}}
-                            />
-                            {validationErrors.confirmPassword && (
-                                <Text color="red.500" fontSize="sm">{validationErrors.confirmPassword}</Text>
-                            )}
-
-                            <Box width={{base: "100%", lg: "80%"}} textAlign="center" mt="1em" >
-                                <Button variant="solid" bg={'blue.800'} _hover={{bg: "blue.700"}} width="100%"
-                                    onClick={handleSubmit}
-                                >
-                                    <Text fontWeight={"bold"}>
-                                        REGISTER
-                                    </Text>
-                                </Button>
-                            </Box>
-                            <Text textAlign={"left"} color={'gray.500'} pt={4}>Already have an account? <Text as={Link} to={'/login'} textDecoration="underline">Login</Text></Text>
-                            {/* <GoogleLogin 
-                                onSuccess={(credentialResponse) => {
-                                    const decoded = jwtDecode(credentialResponse.credential);
-                                    console.log(decoded);
-                                }}
-                                onError={() => {
-                                    console.log("Login Failed");
-                                }}
-
-                            /> */}
+                        /> */}
                     </VStack>
                 </Container>
+
                 <Box 
                     display={{ base: "none", md: "flex" }}
-                    flex={{base: 0, md: 3 }} // Image section takes more space on large screens
+                    flex={{ base: 0, md: 3 }} // Image section takes more space on large screens
                     alignItems="center"
                     justifyContent="center"
                     bg="gray.100" // Optional for visual clarity
@@ -289,12 +344,9 @@ const RegisterPage = () => {
                         zIndex={1}
                     />
                 </Box>
-
-
             </Flex>
         </Box>
-    )
+    );
 }
 
-export default RegisterPage
-
+export default RegisterPage;
