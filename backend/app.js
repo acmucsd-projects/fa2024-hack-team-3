bd9d63acbfc90d5f916207467b223ae1c3609b1a
yal103 <yal103@ucsd.edu>
2025-01-09 14:56:25 -0800

    cors change

--- backend/app.js
+++ backend/app.js
@@ -72,7 +72,11 @@ const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+    {
+        origin: 'http://localhost:5000'
+    }
+));
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
 app.use('/api/posts', commentRoutes);
